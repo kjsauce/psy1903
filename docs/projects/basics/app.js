6b5bb4fbffd0e735581f8e994cdf47d4f0ecbee5
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-09-10 16:27:39 -0400

    In Class Work 9/10

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+response1 = prompt('What is 5 + 3?');
+console.log(response1);
+
+response2 = prompt('What is 5 + 5?');
+console.log(response2);
+// order of operations usually top down //don't have to use let, but a good idea to use it
