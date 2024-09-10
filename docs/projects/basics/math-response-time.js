09a54761c99aa5d25fee9504af2ffa8ededd9098
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-09-10 17:47:38 -0400

    Q6

---
+++ docs/projects/basics/math-response-time.js
@@ -0,0 +1,23 @@
+// Welcome alert saying the phrase given
+alert('In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.')
+
+// Prompt needs to give an equation to solve
+response1 = prompt("What is 3 + 5 ?")
+console.log(response1);
+// Alert tells the respondent their answer
+alert('You answered ' + response1)
+
+// Prompt needs to give an equation to solve
+response2 = prompt("What is 5 + 2 ?")
+console.log(response2);
+// Alert tells the respondent their answer
+alert('You answered ' + response2)
+
+// Prompt needs to give an equation to solve
+response3 = prompt("What is 6 + 4 ?")
+console.log(response3);
+// Alert tells the respondent their answer
+alert('You answered ' + response3)
+
+alert("Thank you for your participation!")
+
