31a4214a82436d7f509ad6c79f23e05dbddf782c
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-09-17 17:36:00 -0400

    q8

--- docs/projects/basics/week3-misc-practice/app.js
+++ docs/projects/basics/week3-misc-practice/app.js
@@ -1,26 +1,37 @@
-let form = document.getElementsByTagName('form')[0];
-let results = document.getElementById('results');
+//let form = document.getElementsByTagName('form')[0];
+//let results = document.getElementById('results');
 
-let num1output = document.getElementById('num1');
-let num2output = document.getElementById('num2');
+//let num1output = document.getElementById('num1');
+//let num2output = document.getElementById('num2');
 
-let num1 = Math.floor(Math.random() * 10) + 1;
-let num2 = Math.floor(Math.random() * 10) + 1;
+//let num1 = Math.floor(Math.random() * 10) + 1;
+//let num2 = Math.floor(Math.random() * 10) + 1;
 
-num1output.innerHTML = num1;
-num2output.innerHTML = num2;
+//num1output.innerHTML = num1;
+//num2output.innerHTML = num2;
 
-equation.innerHTML = 'What is ' + num1 + " + " + num2 + ' ?';
-let response = prompt(equation.innerHTML)
+// equation.innerHTML = 'What is ' + num1 + " + " + num2 + ' ?';
+//let response = prompt(equation.innerHTML)
 
-let feedback = '';
+//let feedback = '';
 
-if (response == num1 + num2) {
-    feedback = 'Correct! ';
-} else if (response == (num1 + num2) - 1 || response == (num1 + num2) + 1) {
-    feedback = 'You were close! ';
-} else {
-    feedback = 'Incorrect.';
+//if (response == num1 + num2) {
+// feedback = 'Correct! ';
+//} else if (response == (num1 + num2) - 1 || response == (num1 + num2) + 1) {
+// feedback = 'You were close! ';
+//} else {
+// feedback = 'Incorrect.';
+//}
+
+//alert(feedback + 'The expected answer is ' + (num1 + num2) + '.');
+
+let age = prompt('How old are you?');
+if (age < 12) {
+    alert('Child');
 }
-
-alert(feedback + 'The expected answer is ' + (num1 + num2) + '.');
+if (age >= 12 || age <= 18) {
+    alert('Teenager');
+}
+if (age >= 18) {
+    alert('Adult');
+}
