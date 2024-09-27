6fb273bc45ea5ceb9f3514330e9dc89cf36f4f52
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-09-27 13:26:20 -0400

    Q3

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,8 +1,20 @@
+// function celsiusToFahrenheit(C, F) {
+// let fahreneit = (F = (C * 1.8) + 32);
+// return fahreneit;
+// }
 
-function celsiusToFahrenheit(C, F) {
-    let fahreneit = (F = (C * 1.8) + 32);
-    return fahreneit;
+// console.log(celsiusToFahrenheit(10)); // Expected output: 50 (yes)
+// console.log(celsiusToFahrenheit(-5)); // 23 (yes)
+
+function convertTemp(temp, convertTo) {
+    let fahreneit = ((temp * 1.8) + 32);
+    let celsius = ((temp - 32) / 1.8);
+    if (convertTo == 'c') {
+        return celsius;
+    } else if (convertTo == 'f') {
+        return fahreneit;
+    }
 }
 
-console.log(celsiusToFahrenheit(10)); // Expected output: 50 (yes)
-console.log(celsiusToFahrenheit(-5)); // 23 (yes)
+console.log(convertTemp(10, 'c')); // Expected output: -12.222222222222221
+console.log(convertTemp(10, 'f')); // Expected output: 50
--- outside-resources/week4.md
+++ outside-resources/week4.md
@@ -1,7 +1,7 @@
 Outside Resources Log - Week 4
 
 AI Prompts
-
+- What is an argument and how can you use it in JavaScript?
 -  
 
 Outside sites
