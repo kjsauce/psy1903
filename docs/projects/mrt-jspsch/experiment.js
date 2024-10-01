a8258361cae3cc37458426c0a3b71df6a2d616bd
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-10-01 17:44:39 -0400

    q4

--- docs/projects/mrt-jspsch/experiment.js
+++ docs/projects/mrt-jspsch/experiment.js
@@ -2,20 +2,52 @@ let jsPsych = initJsPsych();
 
 let timeline = [];
 
-let ageTrial = {
-    type: jsPsychSurveyHtmlForm,
-    preamble: '<p>How old are you?</p>',
-    html: `<p><input type='text' name='age' id='age'></p>`,
-    autofocus: 'age', // id of the field we want to auto-focus on when the trial starts
-    button_label: 'Submit Answer',
-    data: {
-        collect: true,
-    },
-    on_finish: function (data) {
-        data.age = data.response.age;
+// Welcome 
+let welcomeTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Welcome to the Math Response Time Task!</h1> 
+
+    <p>In this experiment, you will be shown a series of math questions.</p>
+    <p>Please answer as quickly and accurately as possible.</p>
+    <p>Press SPACE to begin the first part.</p>
+    `,
+    choices: [' '],
+};
+timeline.push(welcomeTrial);
+
+// Age Trials Begin
+
+for (let condition of conditions) {
+
+    let mathTrial = {
+        type: jsPsychSurveyHtmlForm,
+        preamble: `<p> What is ${condition.numb1} + ${condition.numb2} ?`,
+        html: `<p><input type='text' name='response' id='response'></p>`,
+        autofocus: 'response', // id of the field we want to auto-focus on when the trial starts
+        button_label: 'Submit Answer',
+        data: {
+            collect: true,
+            numb1: condition.numb1,
+            numb2: condition.numb2,
+            answer: condition.answer,
+        },
+        on_finish: function (data) {
+            data.response = data.response.response;
+            if (data.response == data.answer) {
+                data.correct = true;
+            } else if (data.response !== data.answer) {
+                data.correct = false;
+            } else {
+                data.correct = false;
+            }
+        }
+
     }
+    timeline.push(mathTrial);
 }
-timeline.push(ageTrial);
+
+
 
 // Debrief
 let debriefTrial = {
@@ -29,7 +61,7 @@ let debriefTrial = {
         let data = jsPsych.data
             .get()
             .filter({ collect: true })
-            .ignore(['response', 'stimulus', 'trial_type', 'trial_index', 'plugin_version', 'collect'])
+            .ignore(['collect', 'trial_type', 'trial_index', 'plugin_version'])
             .csv();
         console.log(data);
     }
--- outside-resources/week5.md
+++ outside-resources/week5.md
@@ -1,7 +1,7 @@
 Outside Resources Log - Week 5
 
 AI Prompts
-- 
+- How can you take a variable created in another js file and add it to your current one?
 
 Outside sites
 - 
