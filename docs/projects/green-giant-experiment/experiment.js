7523f944ce4703ff0b4013695a488bf00ce9e5a0
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-10-28 19:11:00 -0400

    intro+priming

--- docs/projects/green-giant-experiment/experiment.js
+++ docs/projects/green-giant-experiment/experiment.js
@@ -2,89 +2,122 @@ let jsPsych = initJsPsych();
 
 let timeline = [];
 
-// Welcome 
+// Welcome Trial
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1> Welcome to the Green Giant Experiment!</h1> 
-
+    <h1 class='welcome'> Welcome to the <span class= 'greengiant'>Green Giant Experiment</span>!</h1> 
     <p>In this experiment, you will complete the following three tasks:</p>
-    <li> In Task 1, you will be asked to watch a short video.</li>
-    <li> In Task 2, you will answer a brief set of questions.</li>
-    <li> In Task 3, you will be asked to categorize a series of words. </li>
+    <div class = "task-box">
+         <li> In Task 1, you will be asked to watch a short video.</li>
+         <li> In Task 2, you will answer a brief set of questions.</li>
+         <li> In Task 3, you will be asked to categorize a series of words. </li>
 </div>
     <p>Press <span class='key'>SPACE</span> to begin.</p>
     `,
     choices: [' '],
 };
+
 timeline.push(welcomeTrial);
 
-let inductionTrial = {
+// Task 1 - Priming 
+
+// Setting up the YouTube Videos for Randomization 
+let videos = {
+    video1: {
+        iframe: "<iframe title= 'YouTube video player' width = '640' height='390' src='https://www.youtube.com/embed/XMcab1MFaLc' frameborder='0' allowFullScreen></iframe>",
+        label: 'video1'
+    },
+    video2: {
+        iframe: "<iframe title='YouTube video player' width='640' height='390' src='https://www.youtube.com/embed/aOusNCEXFZw' frameborder='0' allowFullScreen></iframe>",
+        label: 'video2'
+    }
+};
+
+let selectedVideo = Math.random() < 0.5 ? videos.video1 : videos.video2;
+
+// Text on Screen for Priming Task
+let primingTask = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
     <h1> Task 1 of 3</h1> 
-
     <p>Please watch the following video. When you are done, press the <span class='key'>SPACE</span> key to move to the next task. </p>
+    <p>${selectedVideo.iframe}</p>
     `,
     choices: [' '],
+    data: {
+        collect: true,
+        trialType: 'prime',
+        displayedVideo: selectedVideo.label
+    }
 };
-timeline.push(inductionTrial);
 
-let likert_scale = [
-    "Strongly Disagree",
+timeline.push(primingTask);
+
+// Task 2 - Questionnaire 
+
+// let likertscaleA = [
+"Strongly Disagree (1)",
     "Disagree",
-    "Neutral",
+    "Neutral (3)",
     "Agree",
-    "Strongly Agree"
+    "Strongly Agree (5)"
+// ];
+
+// let likertscaleB = [
+    // "Never (1)",
+   // "Very Rarely",
+   // "Ocasionally (3)",
+  //  "Frequently",
+   // "Very Frequently (5)",
 ];
 
-
-let questionnaire = {
+let questionnaireA = {
     type: jsPsychSurveyLikert,
-    questions: [
-        { prompt: "I enjoy solving math problems.", name: 'Math Problem', labels: likert_scale },
-        { prompt: "I find math easy.", name: 'Easy', labels: likert_scale },
+    //  questions: [
+    //  { prompt: "I enjoy solving math problems.", name: 'Math Problem', labels: likert_scale },
+    // { prompt: "I find math easy.", name: 'Easy', labels: likert_scale },
 
-    ],
-    randomize_question_order: false
-};
+    //],
+    // randomize_question_order: false
+    //};
 
-timeline.push(questionnaire);
+    ////timeline.push(questionnaire);
 
-for (let block of conditions) {
+    ///for (let block of conditions) {
 
     // Screen with instructions, indicating the two categories
     // and the expected keys to be pressed
-    let leftCategory = block.categories[0];
-    let rightCategory = block.categories[1];
+    // let leftCategory = block.categories[0];
+    // let rightCategory = block.categories[1];
 
-    for (let trial of block.trials) {
-        // Screen that displays trial.word in the center
-        // as well as the left/right categories
-        // listening for key response (f,j)
-        // on_finish: process the response, store the appropriate data
+    // for (let trial of block.trials) {
+    // Screen that displays trial.word in the center
+    // as well as the left/right categories
+    // listening for key response (f,j)
+    // on_finish: process the response, store the appropriate data
 
-        let example = {
-            type: jsPsychHtmlKeyboardResponse,
-            stimulus: `...`,
-            data: {
-                collect: true,
-                trialType: 'iat',
-                word: trial.word,
-                expectedCategory: trial.expectedCategory,
-                expectedCategoryAsDisplayed: trial.expectedCategoryAsDisplayed,
-                leftCategory: leftCategory,
-                rightCategory: rightCategory
-            },
-            on_finish: function (data) {
-                // if data.response == trial.expectedResponse
-                // data.correct = true
-                // else
-                // data.correct = false
-            }
-        }
-        timeline.push(example);
-    }
-}
+    // let example = {
+    //  type: jsPsychHtmlKeyboardResponse,
+    //  stimulus: `...`,
+    //  data: {
+    //      collect: true,
+    //     trialType: 'iat',
+    //     word: trial.word,
+    //      expectedCategory: trial.expectedCategory,
+    //      expectedCategoryAsDisplayed: trial.expectedCategoryAsDisplayed,
+    //      leftCategory: leftCategory,
+    //     rightCategory: rightCategory
+    //   },
+    //  on_finish: function (data) {
+    // if data.response == trial.expectedResponse
+    // data.correct = true
+    // else
+    // data.correct = false
+    // }
+    // }
+    //  timeline.push(example);
+    //  }
+    // }
 
-jsPsych.run(timeline);
+    jsPsych.run(timeline);
