515e53a00a3221d571e6b94ee8c5a12d084c2322
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-10-28 12:38:01 -0400

    sd

--- docs/projects/lexical-decision/experiment.js
+++ docs/projects/lexical-decision/experiment.js
@@ -113,8 +113,8 @@ let resultsTrial = {
     on_start: function () {
         //  ⭐ Update the following three values as appropriate ⭐
         let prefix = 'lexical-decision';
-        let dataPipeExperimentId = 'your-experiment-id-here';
-        let forceOSFSave = false;
+        let dataPipeExperimentId = '0qB4cs3GAXma';
+        let forceOSFSave = true;
 
         // Filter and retrieve results as CSV data
         let results = jsPsych.data
