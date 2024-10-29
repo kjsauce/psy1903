13261f931b9cc0fc1ef5b1f34a14f2cf8cc676ff
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-10-29 12:13:35 -0400

    WK9 P1

--- docs/projects/lexical-decision/experiment.js
+++ docs/projects/lexical-decision/experiment.js
@@ -114,7 +114,7 @@ let resultsTrial = {
         //  ⭐ Update the following three values as appropriate ⭐
         let prefix = 'lexical-decision';
         let dataPipeExperimentId = '0qB4cs3GAXma';
-        let forceOSFSave = true;
+        let forceOSFSave = false;
 
         // Filter and retrieve results as CSV data
         let results = jsPsych.data
@@ -127,33 +127,14 @@ let resultsTrial = {
 
         // Generate a participant ID based on the current timestamp
         let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+        let fileName = prefix + '-' + participantId + '.csv';
 
-        // Dynamically determine if the experiment is currently running locally or on production
-        let isLocalHost = window.location.href.includes('localhost');
-
-        let destination = '/save';
-        if (!isLocalHost || forceOSFSave) {
-            destination = 'https://pipe.jspsych.org/api/data/';
-        }
-
-        // Send the results to our saving end point
-        fetch(destination, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: '*/*',
-            },
-            body: JSON.stringify({
-                experimentID: dataPipeExperimentId,
-                filename: prefix + '-' + participantId + '.csv',
-                data: results,
-            }),
-        }).then(data => {
-            console.log(data);
+        saveResults(fileName, results, dataPipeExperimentID, forceOSFSave).then(response => {
             jsPsych.finishTrial();
         })
     }
 }
+
 timeline.push(resultsTrial);
 
 // Showed word or psuedo word (on repeat) 
