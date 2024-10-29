f8298039df98006db6f27e83263008853fcf6c1e
Kayla Saucedo <kjsaucedo@college.harvard.edu>
2024-10-29 14:32:16 -0400

    vid

--- docs/projects/lexical-decision/experiment.js
+++ docs/projects/lexical-decision/experiment.js
@@ -1,67 +1,82 @@
-let jsPsych = initJsPsych();
+let jsPsych = initJsPsych({
+    show_progress_bar: true
+});
 
 let timeline = [];
 
-// Welcome 
+//let queryString = new URLSearchParams(window.location.search);
+//let qualtricsId = queryString.get('qualtricsId');
+//console.log(qualtricsId);
+//jsPsych.data.addProperties({ qualtricsId: qualtricsId });
+
+
+let ageCheckTrial = {
+    type: jsPsychSurveyHtmlForm,
+    html: `
+    <h1>Welcome!</h1> 
+    Please enter your age to continue: <input type='text' name='age' id='age'>
+    `,
+    autofocus: 'age',
+    on_finish: function (data) {
+        if (data.response.age < 18) {
+            jsPsych.abortExperiment('You must be 18 years or older to complete this experiment.');
+        }
+    }
+}
+timeline.push(ageCheckTrial);
+
+let enterFullScreenTrial = {
+    type: jsPsychFullscreen,
+    fullscreen_mode: true
+};
+
+timeline.push(enterFullScreenTrial);
+
+/**
+ * Welcome
+ */
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1>Welcome to the Lexical Decision Task!</h1> 
-
+    <h1 class='instructions'>Welcome to the Lexical Decision Task</h1>
     <p>In this experiment, you will be shown a series of characters and asked to categorize whether the characters make up a word or not.</p>
     <p>There are three parts to this experiment.</p>
-    <p>Press SPACE to begin the first part.</p>
+    <p class='instructions'>Press <span class='key'>SPACE</span> to begin the first part.</p>
     `,
-    choices: [' '],
+    choices: [' ']
 };
 timeline.push(welcomeTrial);
 
-let trial = {
+
+
+let primeTrial = {
     type: jsPsychHtmlKeyboardResponse,
-    choices: ['f', 'j'],
     stimulus: `
-        <span class='category1'> <strong>family</strong> (press F)</span>
-        <span class='category2'> <strong>career</strong> (press J)</span>
-        <p class='word'>home</p>`
-    ,
-};
-
-// Create an array of conditions
-let conditions = [
-    {
-        title: 'Part 1',
-        count: 3,
-        conditions: [
-            { characters: 'cat', isWord: true },
-            { characters: 'pin', isWord: true },
-            { characters: 'jgb', isWord: false },
-            { characters: 'mub', isWord: false },
-        ],
+        <p>You were randomly chosen to see this trial.</p> 
+        <p>Press the <span class='key'>SPACE</span> key to continue.</p>
+        `,
+    choices: [' '],
+    data: {
+        collect: true,
+        trialType: 'prime',
     },
-    {
-        title: 'Part 2',
-        count: 4,
-        conditions: [
-            { characters: 'food', isWord: true },
-            { characters: 'burn', isWord: true },
-            { characters: 'mnut', isWord: false },
-            { characters: 'plut', isWord: false },
-        ]
-    },
-    {
-        title: 'Part 3',
-        count: 5,
-        conditions: [
-            { characters: 'apple', isWord: true },
-            { characters: 'jumps', isWord: true },
-            { characters: 'pilde', isWord: false },
-            { characters: 'kandy', isWord: false },
-        ]
+    on_load: function () {
+        if (getRandomNumber(0, 1) == 0) {
+            jsPsych.data.addProperties({ sawPrime: false });
+            jsPsych.finishTrial();
+        } else {
+            jsPsych.data.addProperties({ sawPrime: true });
+        }
     }
-];
+}
+timeline.push(primeTrial);
 
 
+/**
+ * Condition trials
+ */
 for (let block of conditions) {
+
     let blockConditions = jsPsych.randomization.repeat(block.conditions, 1);
 
     let blockIntroTrial = {
@@ -69,9 +84,9 @@ for (let block of conditions) {
         stimulus: `
             <h1>${block.title}</h1>
             <p>You are about to see a series of ${block.count} characters.</p>
-            <p>If the characters make up a word, press the F key.</p>
-            <p>If the characters do not make up a word, press the J key.</p>
-            <p class ='instructions'> Press SPACE to begin.</p>
+            <p>If the characters make up a word, press the <span class='key'>F</span> key.</p>
+            <p>If the characters do not make up a word, press the <span class='key'>J</span> key.</p>
+            <p>Press the <span class='key'>SPACE</span> key to begin.</p>
             `,
         choices: [' '],
     };
@@ -80,13 +95,12 @@ for (let block of conditions) {
     for (let condition of blockConditions) {
         let conditionTrial = {
             type: jsPsychHtmlKeyboardResponse,
-            stimulus: `<h1> ${condition.characters} </h1>`,
+            stimulus: `<h1>${condition.characters}</h1>`,
             choices: ['f', 'j'],
             data: {
                 collect: true,
                 characters: condition.characters,
                 blockId: block.title
-
             },
             on_finish: function (data) {
                 if (data.response == 'f' && condition.isWord == true) {
@@ -99,21 +113,42 @@ for (let block of conditions) {
             }
         }
         timeline.push(conditionTrial);
+
+        let feedbackTrial = {
+            type: jsPsychHtmlKeyboardResponse,
+            stimulus: `<h1>Incorrect</h1>`,
+            trial_duration: 1000,
+            choices: ['NO KEY'],
+            on_load: function () {
+                let lastTrialData = jsPsych.data.getLastTrialData().values()[0];
+                if (lastTrialData.correct) {
+                    // Force skip this feedback trial if they got the previous trial correct
+                    jsPsych.finishTrial();
+                }
+            },
+        }
+        timeline.push(feedbackTrial);
     }
 }
 
+
+
+/**
+ * Results
+ */
 let resultsTrial = {
     type: jsPsychHtmlKeyboardResponse,
     choices: ['NO KEYS'],
     async: false,
     stimulus: `
         <h1>Please wait...</h1>
+        <span class='loader'></span>
         <p>We are saving the results of your inputs.</p>
         `,
     on_start: function () {
         //  ⭐ Update the following three values as appropriate ⭐
         let prefix = 'lexical-decision';
-        let dataPipeExperimentId = '0qB4cs3GAXma';
+        let dataPipeExperimentId = 'xGrIMXyGYhic';
         let forceOSFSave = false;
 
         // Filter and retrieve results as CSV data
@@ -126,29 +161,44 @@ let resultsTrial = {
         console.log(results);
 
         // Generate a participant ID based on the current timestamp
-        let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+        let participantId = getCurrentTimestamp();
+
         let fileName = prefix + '-' + participantId + '.csv';
 
-        saveResults(fileName, results, dataPipeExperimentID, forceOSFSave).then(response => {
+        saveResults(fileName, results, dataPipeExperimentId, forceOSFSave).then(response => {
             jsPsych.finishTrial();
         })
+
+
     }
 }
-
 timeline.push(resultsTrial);
 
-// Showed word or psuedo word (on repeat) 
+
+
+let exitFullScreenTrial = {
+    type: jsPsychFullscreen,
+    fullscreen_mode: false
+};
+timeline.push(exitFullScreenTrial);
+
+
+/**
+ * Debrief
+ */
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1> Thank you! </h1>
-    <p> You can now close this tab </p>
+    <h1>Thank you!</h1>
     `,
     choices: ['NO KEYS'],
-
+    on_start: function () {
+        jsPsych.progressBar.progress = 1;
+    }
 }
 timeline.push(debriefTrial);
 
-// Debrief
+
+
 
 jsPsych.run(timeline);
